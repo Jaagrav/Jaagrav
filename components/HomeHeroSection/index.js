93b3c044e0049ad1299806575230a44f7185d29d
jaagrav <jaagravseal03@gmail.com>
2022-04-20 23:49:07 +0530

    Added transition to theme change on hero section

--- components/HomeHeroSection/index.js
+++ components/HomeHeroSection/index.js
@@ -17,19 +17,20 @@ export default function HomeHeroSection() {
   return (
     <div className="h-screen w-full relative">
       <div
-        className="h-full w-full absolute bg-center bg-no-repeat bg-cover hidden dark:block"
+        className="h-full w-full absolute bg-center bg-no-repeat bg-cover transition dark:duration-300 opacity-0 dark:opacity-100"
         style={{
           backgroundImage: `url(./assets/images/hero-section-bg-dark.webp)`,
         }}
       ></div>
       <div
-        className="h-full w-full absolute bg-center bg-no-repeat bg-cover dark:hidden block"
+        className="h-full w-full absolute bg-center bg-no-repeat bg-cover transition dark:duration-300 dark:opacity-0 opacity-100"
         style={{
           backgroundImage: `url(./assets/images/hero-section-bg-light.webp)`,
         }}
       ></div>
       <canvas className="h-full w-full absolute" id="liquid-canvas"></canvas>
-      <div className="absolute h-full w-full bg-gradient-to-b from-transparent to-white dark:to-bgColor translate-y-1 pointer-events-none"></div>
+      <div className="absolute h-full w-full bg-gradient-to-b from-transparent transition duration-300 to-white translate-y-1 pointer-events-none dark:opacity-0 opacity-100"></div>
+      <div className="absolute h-full w-full bg-gradient-to-b from-transparent transition duration-300 to-bgColor translate-y-1 pointer-events-none opacity-0 dark:opacity-100"></div>
       <div className="absolute w-full h-full pointer-events-none">
         <div className="skew max-w-screen-xl absolute top-0 left-0 bottom-0 right-0 m-auto z-10 h-fit w-fit px-10 md:px-24">
           <div className="md:px-20">

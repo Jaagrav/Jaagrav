6fdc5f3223957581c794bcb850aae5a208391e4f
jaagrav <jaagravseal03@gmail.com>
2022-04-30 11:37:31 +0530

    Merged rebased crap

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import {
   GallerySection,
   SubHeader,
@@ -60,19 +62,19 @@ export default function About() {
       </Head>
       <Animatify>
         <div className="px-10 md:px-32 pb-12">
-          <div className="md:grid md:grid-cols-[1fr_0.4fr] gap-8 max-w-screen-xl m-auto">
+          <div className="md:grid md:grid-cols-[1fr_0.4fr] gap-8 max-w-screen-xl m-auto mt-4">
             {/* <div className="block md:hidden">
               <div
                 className="relative z-10 bg-contain bg-black bg-no-repeat bg-center w-full h-[50vw]"
                 style={{ backgroundImage: "url(/assets/images/jaagrav.webp)" }}
               ></div>
             </div> */}
-            <div className="-translate-y-1/4 mt-12 md:m-auto pointer-events-none">
-              <h1 className="text-border text-[6rem] md:text-[7rem] whitespace-nowrap opacity-10 translate-y-1/2">
+            <div className="md:m-auto">
+              <h1 className="absolute text-border text-[6rem] md:text-[7rem] whitespace-nowrap opacity-10 -translate-y-1/2">
                 Jaagrav Seal
               </h1>
               <img
-                className="absolute m-auto top-0 bottom-0 left-0 -translate-x-1/2 max-w-none w-[50rem] md:w-[75rem] dark:opacity-60"
+                className="absolute m-auto top-0 left-0 -translate-y-1/2 -translate-x-1/2 max-w-none w-[50rem] md:w-[75rem] dark:opacity-60"
                 src="/assets/images/blur-ball-blue.webp"
               />
               <div className="relative z-10">
@@ -84,8 +86,15 @@ export default function About() {
                   designing and developing websites, I am currently 18 years
                   old, and am also in the first year of my college. I am
                   currently pursuing B. Tech in Computer Science and Engineering
-                  from University.
+                  from KIIT University.
                 </span>
+                <Link href="/resume">
+                  <a
+                    className={`inline-block mt-8 w-full md:w-fit text-center md:text-left md:mr-4 px-8 py-2 text-xl border-lightTextColor dark:border-white border-2 bg-lightTextColor dark:bg-white text-white dark:text-bgColor rounded-xl transition shadow-none hover:shadow-xl hover:scale-105`}
+                  >
+                    Check out Resume
+                  </a>
+                </Link>
               </div>
             </div>
             <div className="hidden md:block">
@@ -97,16 +106,29 @@ export default function About() {
           </div>
         </div>
         <GallerySection />
-        <SubHeader
-          title="Why hire me?"
-          caption="Here's a list of all my skills and all the reasons why you should hire me. As a developer, I don't completely know anything, you see I am not very good at memorizing, I normally read documentations everytime I am building something, but I make sure to get the work done in time."
-        />
+        <SubHeader title="Why hire me?" />
         <SkillsSection />
         <SubHeader
           title="My Experience"
           caption="I am a goddamn professional. So long I have done some internships, have worked for some clients, and have also contributed to OpenSource bootcamps, communities and programs that you can read more about below."
         />
         <ExperienceSection />
+        <SubHeader
+          title="People love me, seriously."
+          caption="The word testimonial comes from the latin word testimonium which means a formal statement testifying to someone's character and qualifications. Basically this section highlights some of the beautiful things, people I admire and respect, say about me."
+        />
+        <TestimonialsSection />
+        <span className="relative z-10 block text-center mx-auto mt-2 mb-16 text-lightTextColor dark:text-white text-xl skew">
+          {"All of these are totally real, "}
+          <Link href="https://www.linkedin.com/in/jaagrav/details/recommendations/">
+            <a
+              target="_blank"
+              className="transition text-pink hover:underline underline-offset-8"
+            >
+              look here for proof.
+            </a>
+          </Link>{" "}
+        </span>
       </Animatify>
     </div>
   );

41c66c396916815f9687757790ba3b57805d465e
jaagrav <jaagravseal03@gmail.com>
2022-04-30 11:38:56 +0530

    Merged rebased crap

--- components/index.js
+++ components/index.js
@@ -10,3 +10,4 @@ export { default as TestimonialsSection } from "./TestimonialsSection";
 export { default as PageTransition } from "./PageTransition";
 export { default as Animatify } from "./Animatify";
 export { default as FeaturedArticle } from "./FeaturedArticle";
+export { default as Animatify } from "./Animatify";

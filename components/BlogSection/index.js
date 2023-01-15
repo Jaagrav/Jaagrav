ad4257c4de001f5c399bc0c282a081a837b1e338
Jaagrav <contact@jaagrav.in>
2023-01-16 02:42:02 +0530

    fix: blog component mobile ui

--- components/BlogSection/index.js
+++ components/BlogSection/index.js
@@ -41,7 +41,7 @@ export default function BlogSection() {
 }
 
 export function Blog({file, metadata}) {
-    return <div className="w-[36rem] pr-12">
+    return <div className="w-[36rem] px-10 md:pl-0 md:pr-12">
             <div><div className='rounded-2xl h-72 w-full bg-cover bg-center' style={{background: `url('https://blog.jaagrav.in/assets/blogs-media/${file}/thumbnail.webp')`}}></div></div>
             <div>
                 <div className="text-3xl font-bold mt-4 text-lightTextColor dark:text-white line-clamp-1">{metadata.title}</div>
